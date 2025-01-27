5fc46c8b24917e19497cfe2663a041d3e564c881
Bobby Johnson <bobby@notmyself.io>
2025-01-27 08:59:45 -0800

    switched commands to use non insiders version of code

--- installs/vscode.ps1
+++ installs/vscode.ps1
@@ -1,4 +1,4 @@
 Get-Content $PSScriptRoot\..\settings/vscode/extensions | ForEach-Object {
   Write-Output "Installing Extension $_"
-  code-insiders --install-extension $_ --force
+  code --install-extension $_ --force
 }
